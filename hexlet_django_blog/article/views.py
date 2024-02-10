953d7e7e942101f75fed536da938b0f782a429fd
Garkusha Valery <aston585v@gmail.com>
2024-02-10 14:05:34 +0300

    feat: урок №14. Просмотр (CRUD)

--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,10 +1,6 @@
-# from django.http import HttpResponse
 from django.views import View
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from hexlet_django_blog.article.models import Article
-
-# def index(request, article_id, tags):
-#     return HttpResponse(f'Статья номер {article_id}. Тег {tags}')
 
 
 class IndexView(View):
@@ -19,3 +15,12 @@ class IndexView(View):
                 'articles': articles,
             }
         )
+
+
+class ArticleView(View):
+
+    def get(self, request, *args, **kwargs):
+        article = get_object_or_404(Article, id=kwargs['id'])
+        return render(request, 'articles/show.html', context={
+            'article': article,
+        })
